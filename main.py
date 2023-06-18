664b21d5167dc46fa48aaedcbb14fa7d38dae107
Matvey Mogilev <mekovalsky@gmail.com>
2023-06-18 17:00:39 +0300

    implemented a sequence of questions from a json file

--- main.py
+++ main.py
@@ -1,49 +1,55 @@
 import telebot
 from telebot import types
+import json
+
 
 with open("token.txt", 'r') as file:
     token = file.readline()
 
+with open("form.json", 'r', encoding="UTF-8") as file:
+    data = json.load(file)
+
 bot = telebot.TeleBot(token)
 bot.delete_webhook()
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	markup = types.ReplyKeyboardMarkup()
-	button1 = types.KeyboardButton('Полезные ссылки')
-	button2 = types.KeyboardButton('Семестровый опрос')
-	button3 = types.KeyboardButton('Обратная связь')
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	button1 = types.KeyboardButton('Семестровый опрос')
+	button2 = types.KeyboardButton('Обратная связь')
 	markup.row(button1)
-	markup.row(button2, button3)
+	markup.row(button2)
 	bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Чем могу помочь?", reply_markup=markup)
 	print(message.from_user.id)
 	bot.register_next_step_handler(message, start)
 
 
 def start(message):
-	if message.text == 'Полезные ссылки':
+	def ask(message):
+		try:
+			quest = next(questions)
+			question = bot.send_message(message.chat.id, quest['text'])
+			bot.register_next_step_handler(question, read_answer)
+
+		except StopIteration:
+			bot.send_message(message.chat.id, "Спасибо за ответы!")
+			bot.register_next_step_handler(message, send_welcome)
+
+	def read_answer(answer):
+		print("answer ->", answer.text)
+		ask(answer)
+
+	if message.text == 'Семестровый опрос':
+		questions = (i for i in data.values())
+		ask(message)
+
+	if message.text == 'Обратная связь':
 		pass
-		markup = types.InlineKeyboardMarkup()
-		button1 = types.InlineKeyboardButton('Канал БМТ 🧬', url="https://t.me/biomedt")
-		button2 = types.InlineKeyboardButton('Электронный университет', url="https://lks.bmstu.ru")
-		markup.row(button1)
-		markup.row(button2)
-		bot.send_message(message.chat.id, f"Куда хочешь перейти?", reply_markup=markup)
-
-	elif message.text == 'Семестровый опрос': # Начало семестрового опросника
-		bot.send_message(message.chat.id, "Cеместровый опрос:")
-		bot.register_next_step_handler(message, semester_form)
-
-	elif message.text == 'Обратная связь': # Начало обращения
-		bot.send_message(message.chat.id, "Жду твою обратную связь:")
-		bot.register_next_step_handler(message, feedback)
 
 
-def semester_form():
-	pass
 
-def feedback():
-	pass
+
 
 
 @bot.message_handler(commands=['help'])
@@ -52,8 +58,7 @@ def help(message):
 
 @bot.message_handler(content_types=['text'])
 def info(message):
-	if message.text.lower().startswith('привет'):
-		bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}")
+	bot.send_message(message.chat.id, f"Напиши /start, чтобы начать")
 
 
 bot.infinity_polling()
